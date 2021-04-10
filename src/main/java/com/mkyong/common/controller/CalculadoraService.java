80db5766f06c51964d77dd662b5e42aeca85c4b7
Angel Muñoz <info@angelmunoz.es>
2021-04-10 13:05:48 +0200

    añadimos servicio calculadora

---
+++ src/main/java/com/mkyong/common/controller/CalculadoraService.java
@@ -0,0 +1,17 @@
+package com.mkyong.common.controller;
+
+public class CalculadoraService {
+   public static int sumar(int a, int b) {
+	   return a+b;
+   }
+   
+	public static int division(int a, int b) {
+		return a / b;
+	}
+	
+	public static int multiplicar(int a, int b) {
+		return a * b;
+	}
+	
+
+}
